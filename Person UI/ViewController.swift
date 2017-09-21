c74c64afae4f73e7ce1ac5ec48f7a71581185e10
Aidan Madden <aidanmadden@Aidans-MacBook-Pro-2.local>
2017-09-21 10:22:57 -0500

    First commit

---
+++ Person UI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Person UI
+//
+//  Created by Aidan Madden on 9/21/17.
+//  Copyright © 2017 Aidan Madden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
